db6c0573a159e437078d9101c600fff16f1e73c4
dhananjay1908 <dhananjay.singh5607@gmail.com>
2022-03-19 19:42:55 +0530

    basic styling done

--- discussion_portal/src/components/LeftComponent/BlogPost.jsx
+++ discussion_portal/src/components/LeftComponent/BlogPost.jsx
@@ -1,4 +1,5 @@
 import { Box, makeStyles, Card, Typography,Button } from "@material-ui/core";
+import { textAlign } from "@mui/system";
 import { useState, useContext } from "react";
 
 const useStyles = makeStyles({
@@ -8,14 +9,20 @@ const useStyles = makeStyles({
     },
     Recent:{
         fontFamily: 'verdana',
-        padding: '12px 12px',
+        padding: '0 12px',
         fontWeight: '500',
         color: 'black',
-        fontSize: '18px'
+        fontSize: '18px',
+        textAlign:"center"
     },
     card: {
         padding: '10px',
-        margin: '10px 0px'
+        margin: '10px 10px',
+        display: "flex",
+        flexDirection:"column"
+    },
+    button: {
+        margin: "10px"
     }
 
 })
@@ -25,21 +32,23 @@ const BlogPost = ()=>{
     return (
         <Box className={classes.container} >
             <Box style={{width: '100%'}} >
-                <Button style={{width: "100%"}} variant="contained" color="primary" >Create New Post</Button>
+                <Button style={{width: "90%",margin:"5% 5% 0 5% "}} variant="contained" color="primary" >Create New Post</Button>
             </Box>
             <Box className={classes.Recent} >
-                Recent Actions: 
+               <h4>Timeline</h4>  
             </Box>
             <Box>
                 <Card className={classes.card} >
-                    <Typography  >Dhananjay Singh</Typography>
-                    <Typography variant="p" > How to Center a div tag </Typography>
+                    <Typography style={{fontWeight:"700"}}>Dhananjay Singh</Typography>
+                    <Typography variant="p"> How to Center a div tag </Typography>
+                    <Button variant="contained" color="primary" className={classes.button}>Enter Discussion</Button>
                 </Card>
             </Box>
             <Box>
                 <Card className={classes.card} >
-                    <Typography  >Dhananjay Singh</Typography>
+                    <Typography style={{fontWeight:"700"}}>Dhananjay Singh</Typography>
                     <Typography variant="p" > How to Center a div..... </Typography>
+                    <Button variant="contained" color="primary" className={classes.button}>Enter Discussion</Button>
                 </Card>
             </Box>
         </Box>
